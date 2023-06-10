480dd3f8406a031eaeb949b479e35b49c0f7dd9f
Kostantin89 <kostiakava@gmail.com>
2023-06-10 15:19:18 +0300

    adding new lines of code

--- src/test/java/com/rest/RequestPayLoadComplexJson.java
+++ src/test/java/com/rest/RequestPayLoadComplexJson.java
@@ -23,7 +23,7 @@ public class RequestPayLoadComplexJson {
     @BeforeClass
     public void beforeClass() {
         RequestSpecBuilder requestSpecBuilder = new RequestSpecBuilder().
-                setBaseUri("https://4fffb901-d716-492d-9620-0ceaca151514.mock.pstmn.io/RequestPayLoadComplexJson").
+                setBaseUri("https://4fffb901-d716-492d-9620-0ceaca151514.mock.pstmn.io").
                 addHeader("x-mock-match-request-body", "true").
                 //        setConfig(config.encoderConfig(EncoderConfig.encoderConfig()
                 //                .appendDefaultContentCharsetToContentTypeIfUndefined(false))).
@@ -46,53 +46,52 @@ public class RequestPayLoadComplexJson {
         idArrayList.add(5);
         idArrayList.add(9);
 
-        HashMap<String,Object> batterHashMap2 = new HashMap<String,Object>();
-        batterHashMap2.put("id",idArrayList);
-        batterHashMap2.put("type","Chocolate");
+        HashMap<String, Object> batterHashMap2 = new HashMap<String, Object>();
+        batterHashMap2.put("id", idArrayList);
+        batterHashMap2.put("type", "Chocolate");
 
-        HashMap<String,Object> batterHashMap1 = new HashMap<String,Object>();
-        batterHashMap1.put("id","1001");
-        batterHashMap1.put("type","Regular");
+        HashMap<String, Object> batterHashMap1 = new HashMap<String, Object>();
+        batterHashMap1.put("id", "1001");
+        batterHashMap1.put("type", "Regular");
 
-        List<HashMap<String,Object>> batterArrayList = new ArrayList<HashMap<String,Object>>();
+        List<HashMap<String, Object>> batterArrayList = new ArrayList<HashMap<String, Object>>();
         batterArrayList.add(batterHashMap1);
         batterArrayList.add(batterHashMap2);
 
-        HashMap <String,List<HashMap<String,Object>>>batterHashMap =
-                new HashMap<String,List<HashMap<String,Object>>>();
-        batterHashMap.put("batter",batterArrayList);
+        HashMap<String, List<HashMap<String, Object>>> battersHashMap
+                = new HashMap<String, List<HashMap<String, Object>>>();
+        battersHashMap.put("batter", batterArrayList);
 
-        List<String> typeArrayList = new ArrayList<>();
+        List<String> typeArrayList = new ArrayList<String>();
         typeArrayList.add("test1");
         typeArrayList.add("test2");
 
-        HashMap<String,Object> toppingHashMap2 = new HashMap<String,Object>();
-        toppingHashMap2.put("id","5002");
-        toppingHashMap2.put("type",typeArrayList);
+        HashMap<String, Object> toppingHashMap2 = new HashMap<String, Object>();
+        toppingHashMap2.put("id", "5002");
+        toppingHashMap2.put("type", typeArrayList);
 
-        HashMap<String,Object> toppingHashMap1 = new HashMap<String,Object>();
-        toppingHashMap1.put("id","5002");
-        toppingHashMap2.put("type","None");
+        HashMap<String, Object> toppingHashMap1 = new HashMap<String, Object>();
+        toppingHashMap1.put("id", "5001");
+        toppingHashMap1.put("type", "None");
 
-
-        List<HashMap<String,Object>> toppingArrayList = new ArrayList<HashMap<String,Object>>();
+        List<HashMap<String, Object>> toppingArrayList = new ArrayList<HashMap<String, Object>>();
         toppingArrayList.add(toppingHashMap1);
         toppingArrayList.add(toppingHashMap2);
 
-        HashMap<String,Object> mainHashMap = new HashMap<String,Object>();
-        mainHashMap.put("id","0001");
-        mainHashMap.put("type","donut");
-        mainHashMap.put("name","Cake");
-        mainHashMap.put("ppu",0.55);
-        mainHashMap.put("batters",batterHashMap);
-        mainHashMap.put("topping",toppingArrayList);
+        HashMap<String, Object> mainHashMap = new HashMap<String, Object>();
+        mainHashMap.put("id", "0001");
+        mainHashMap.put("type", "donut");
+        mainHashMap.put("name", "Cake");
+        mainHashMap.put("ppu", 0.55);
+        mainHashMap.put("batters", battersHashMap);
+        mainHashMap.put("topping", toppingArrayList);
 
         given().
                 body(mainHashMap).
-        when().
-        post("/postComplexJson").
-        then().spec(customResponseSpecification).
-        assertThat().
+                when().
+                post("/postComplexJson").
+                then().spec(customResponseSpecification).
+                assertThat().
         body("Message",equalTo("Success"));
 
     }
--- src/test/java/com/rest/RequestPayLoadComplexJsonTask.java
+++ src/test/java/com/rest/RequestPayLoadComplexJsonTask.java
@@ -10,13 +10,20 @@ import io.restassured.specification.ResponseSpecification;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
 public class RequestPayLoadComplexJsonTask {
     ResponseSpecification customResponseSpecification;
 
     @BeforeClass
     public void beforeClass() {
         RequestSpecBuilder requestSpecBuilder = new RequestSpecBuilder().
-                setBaseUri("https://8cca1bed-9a34-49db-af07-4d1a41b7e7e4.mock.pstmn.io/postComplexJson").
+                setBaseUri("https://8cca1bed-9a34-49db-af07-4d1a41b7e7e4.mock.pstmn.io").
                 addHeader("x-mock-match-request-body", "true").
                 setContentType("application/json;charset=utf-8").
                 log(LogDetail.ALL);
@@ -30,9 +37,60 @@ public class RequestPayLoadComplexJsonTask {
         customResponseSpecification = responseSpecBuilder.build();
 
     }
-        @Test
-        public void validatePostRequestPayloadComplexJsonMyTask() {
 
+    @Test
+    public void validatePostRequestPayloadComplexJsonMyTask() {
+
+
+        List<Integer> rgbColors = new ArrayList<>();
+        rgbColors.add(255);
+        rgbColors.add(255);
+        rgbColors.add(255);
+        rgbColors.add(1);
+
+        HashMap<String, Object> firstColor = new HashMap<>();
+        firstColor.put("rgba", rgbColors);
+        firstColor.put("hex", "#000");
+
+        HashMap<String,Object> mainColor = new HashMap<String,Object>();
+        mainColor.put("color","black");
+        mainColor.put("category","hue");
+        mainColor.put("type","primary");
+        mainColor.put("code",firstColor);
+
+
+        List<Integer> rgbColorsTwo = new ArrayList<>();
+        rgbColorsTwo.add(0);
+        rgbColorsTwo.add(0);
+        rgbColorsTwo.add(0);
+        rgbColorsTwo.add(1);
+
+        HashMap<String, Object> secondaryColor= new HashMap<>();
+        secondaryColor.put("rgba", rgbColorsTwo);
+        secondaryColor.put("hex", "#FFF");
+
+        HashMap<String,Object> secondColor = new HashMap<String,Object>();
+        secondColor.put("color","white");
+        secondColor.put("category","value");
+        secondColor.put("code",secondaryColor);
+
+
+
+        List<HashMap<String, Object>> colorsList = new ArrayList<HashMap<String, Object>>();
+        colorsList.add(mainColor);
+        colorsList.add(secondColor);
+
+
+        HashMap<String, Object> firstMainObjects = new HashMap<String, Object>();
+        firstMainObjects.put("colors", colorsList);
+
+        given().
+                body(firstMainObjects).
+                when().
+                post("/postComplexJson").
+                then().spec(customResponseSpecification).
+                assertThat().
+                body("Message", equalTo("Everything went great! "));
 
     }
 }
